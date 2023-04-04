4b93d1d98f698644a3d621f24e8ce972bb4a4450
Zoltan Balogh <zbal@freemail.hu>
2023-04-04 16:38:29 +0200

    separated config file for Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,8 @@
+import { firebaseConfig } from "./firebase";
+
 export const environment = {
   production: true,
-  firebaseConfig : {
-    apiKey: "AIzaSyAgBzwaMKv7H9Z_rwH_se5o9CMrqgc2j24",
-    authDomain: "reservation-gui.firebaseapp.com",
-    projectId: "reservation-gui",
-    storageBucket: "reservation-gui.appspot.com",
-    messagingSenderId: "550151776440",
-    appId: "1:550151776440:web:f21be0060d09b79a4eec4d",
-    measurementId: "G-DD36X25G30"
-  },
+  // Firebase Configuration
+  ...firebaseConfig,
   NUMBER_OF_ALL_DESKS: 100
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,13 @@
+import { firebaseConfig } from "./firebase";
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false,
-  firebaseConfig : {
-    apiKey: "AIzaSyAgBzwaMKv7H9Z_rwH_se5o9CMrqgc2j24",
-    authDomain: "reservation-gui.firebaseapp.com",
-    projectId: "reservation-gui",
-    storageBucket: "reservation-gui.appspot.com",
-    messagingSenderId: "550151776440",
-    appId: "1:550151776440:web:f21be0060d09b79a4eec4d",
-    measurementId: "G-DD36X25G30"
-  },
+  // Firebase Configuration
+  ...firebaseConfig,
   NUMBER_OF_ALL_DESKS: 100
 };
 
